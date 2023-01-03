679c2099ecd02bf52e0072b14fb9561b6cdebc02
Hyejung Park <hyejj19@naver.com>
2023-01-03 21:35:12 +0900

    📦️ Chore :  Recoil 초기셋팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { RecoilRoot } from 'recoil';
+
 const App = () => {
-  return <div>boilerplate</div>;
-}
+  return (
+    <RecoilRoot>
+      <div>boilerplate</div>
+    </RecoilRoot>
+  );
+};
 
 export default App;
